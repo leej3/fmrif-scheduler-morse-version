721fd452585f4a2446a4d9d568b6eb89e204f982
jimmyfrasche <soapboxcicero@gmail.com>
2021-09-29 14:06:52 -0700

    allow succesful import of historical data

---
+++ sql/002-loosen-schema.sql
@@ -0,0 +1,17 @@
+-- drop constraints that cause import to fail
+
+alter table tblsched drop constraint tblsched_billdeptcode_fkey;
+
+alter table tbltemplate drop constraint tbltemplate_deptcode_fkey;
+
+alter table tbltemplates drop constraint tbltemplates_scannercode_fkey;
+
+alter table technicalscans drop constraint technicalscans_deptcode_fkey;
+
+alter table tblsched drop constraint tblsched_deptcode_fkey;
+
+alter table tbltemplate	drop constraint tbltemplate_scannercode_fkey;
+
+alter table technicalscans drop constraint technicalscans_scannercode_fkey;
+
+alter table tbltemplate drop constraint tbltemplate_instcode_fkey;
---
+++ sql/004-tighten-schema.sql
@@ -0,0 +1,25 @@
+-- add back constraints after import (take on "not valid" to prevent old rows from causing errors)
+
+ALTER TABLE ONLY tblsched
+    ADD CONSTRAINT tblsched_billdeptcode_fkey FOREIGN KEY (billdeptcode) REFERENCES tlkpdept(deptcode) ON UPDATE CASCADE DEFERRABLE not valid;
+
+ALTER TABLE ONLY tbltemplate
+    ADD CONSTRAINT tbltemplate_deptcode_fkey FOREIGN KEY (deptcode) REFERENCES tlkpdept(deptcode) ON UPDATE CASCADE DEFERRABLE not valid;
+
+ALTER TABLE ONLY tbltemplates
+    ADD CONSTRAINT tbltemplates_scannercode_fkey FOREIGN KEY (scannercode) REFERENCES tlkpscanner(scannercode) ON UPDATE CASCADE DEFERRABLE not valid;
+
+ALTER TABLE ONLY technicalscans
+    ADD CONSTRAINT technicalscans_deptcode_fkey FOREIGN KEY (deptcode) REFERENCES tlkpdept(deptcode) not valid;
+
+ALTER TABLE ONLY tblsched
+    ADD CONSTRAINT tblsched_deptcode_fkey FOREIGN KEY (deptcode) REFERENCES tlkpdept(deptcode) ON UPDATE CASCADE DEFERRABLE not valid;
+
+ALTER TABLE ONLY tbltemplate
+    ADD CONSTRAINT tbltemplate_scannercode_fkey FOREIGN KEY (scannercode, templatecode) REFERENCES tbltemplates(scannercode, templatecode) ON UPDATE CASCADE DEFERRABLE not valid;
+
+ALTER TABLE ONLY technicalscans
+    ADD CONSTRAINT technicalscans_scannercode_fkey FOREIGN KEY (scannercode) REFERENCES tlkpscanner(scannercode) not valid;
+
+ALTER TABLE ONLY tbltemplate
+    ADD CONSTRAINT tbltemplate_instcode_fkey FOREIGN KEY (instcode) REFERENCES tlkpinst(instcode) not valid;
