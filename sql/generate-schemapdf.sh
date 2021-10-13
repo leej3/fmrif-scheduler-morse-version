e76579a8ce6226f6e850292ea454b950b506fac2
jimmyfrasche <soapboxcicero@gmail.com>
2021-10-13 16:24:29 -0700

    add legacy schema to ER diagram

--- sql/generate-schemapdf.sh
+++ sql/generate-schemapdf.sh
@@ -5,5 +5,5 @@ set -e
 if ! hash erd 2>/dev/null; then
 	echo >&2 "erd not installed, skipping generation of schema.pdf. Install: https://github.com/BurntSushi/erd"
 else
-	erd -i schema.er -o schema.pdf
+	erd -i schema.er -o schema.pdf -e compound
 fi
