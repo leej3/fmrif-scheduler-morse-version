c7f02b094ffef2f7f583da5bc30ec32596ed783c
jimmyfrasche <soapboxcicero@gmail.com>
2021-10-20 15:51:36 -0700

    simplify presentation of ER diagram

--- sql/generate-schemapdf.sh
+++ sql/generate-schemapdf.sh
@@ -5,5 +5,10 @@ set -e
 if ! hash erd 2>/dev/null; then
 	echo >&2 "erd not installed, skipping generation of schema.pdf. Install: https://github.com/BurntSushi/erd"
 else
-	erd -i schema.er -o ../docs/schema-er-diagram.pdf -e compound
+	erd -i schema.er -f dot -e compound |
+		# make edges easier to read; change style to solid, remove head and tail labels
+		sed 's/style=dashed/style=solid/' |
+		sed 's/headlabel=.*$//g' |
+		sed 's/taillabel=[^]]*//g' |
+		dot -Tpdf -o ../docs/schema-er-diagram.pdf
 fi
