d93f321988dc723f8a1b036b93198f3e60566646
jimmyfrasche <soapboxcicero@gmail.com>
2021-10-29 13:50:05 -0700

    Embed ER in schema docs
    The embedding included all the text in the ER diagram joined together as
    the alt text for the image which was incorrect so care had to be taken
    to remove the text from the ER diagram before embedding.

--- sql/generate-schemapdf.sh
+++ sql/generate-schemapdf.sh
@@ -2,13 +2,35 @@
 
 set -e
 
-if ! hash erd 2>/dev/null; then
-	echo >&2 "erd not installed, skipping generation of schema.pdf. Install: https://github.com/BurntSushi/erd"
-else
-	erd -i schema.er -f dot -e compound |
-		# make edges easier to read; change style to solid, remove head and tail labels
-		sed 's/style=dashed/style=solid/' |
-		sed 's/headlabel=.*$//g' |
-		sed 's/taillabel=[^]]*//g' |
-		dot -Tpdf -o ../docs/schema-er-diagram.pdf
+has() {
+	hash "$1" 2>/dev/null
+}
+
+fail() {
+	echo "$@" 1>&2
+	exit 0
+}
+
+if ! has erd; then
+	fail "erd not installed, skipping generation of ER diagram. Install: https://github.com/BurntSushi/erd"
 fi
+if ! has dot; then
+	fail "graphviz is required to format ER diagram"
+fi
+if ! has inkscape; then
+	fail "inkscape is require to postprocess ER diagram"
+fi
+
+erd -i schema.er -f dot -e compound |
+	# make edges easier to read; change style to solid, remove head and tail labels
+	sed 's/style=dashed/style=solid/' |
+	sed 's/headlabel=.*$//g' |
+	sed 's/taillabel=[^]]*//g' |
+	# inkscape can't handle underlines properly (!?)
+	sed 's@<U>@<B>@g' | sed 's@</U>@</B>@g' |
+	dot -Tsvg |
+	# fix title
+	sed 's@<title>%3</title>@<title>ER diagram</title>@' |
+	# pandoc embeds text from svg as a single string joined without spaces as alt for figure
+	# this is insane, so we have to reduce the text to paths
+	inkscape -p --export-text-to-path -o ../docs/er-diagram.svg
