810638be693cf0a5ea6d01b5aaad9910ed4217ed
jimmyfrasche <soapboxcicero@gmail.com>
2021-11-18 11:16:27 -0800

    remove unused variable

--- scheduler/app.py
+++ scheduler/app.py
@@ -40,7 +40,6 @@ with app.open_resource("config.json", "r") as f:
 
 
 db.init_app(app)
-app.config["SESSION_SQL_ALCHEMY"] = db
 Session(app)
 message.mailer.init_app(app)
 signer = URLSafeSerializer(app.config["SECRET_KEY"], salt="nih-scheduler")
