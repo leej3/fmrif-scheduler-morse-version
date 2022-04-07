061be7adbe06c79fcb5a1c28d191f0e936822b16
jimmyfrasche <soapboxcicero@gmail.com>
2022-04-07 11:19:17 -0700

    allow smtp sends to fail inband

--- scheduler/message.py
+++ scheduler/message.py
@@ -1,3 +1,4 @@
+from smtplib import SMTPException
 from typing import Optional
 
 from flask import current_app
@@ -6,7 +7,7 @@ from flask_mail import Mail, Message
 mailer = Mail()
 
 
-def send(to: str, subj: str, msg: str, sender: Optional[str] = None) -> None:
+def send(to: str, subj: str, msg: str, sender: Optional[str] = None) -> bool:
     m = Message()
     m.subject = to
     m.body = msg
@@ -17,6 +18,11 @@ def send(to: str, subj: str, msg: str, sender: Optional[str] = None) -> None:
     # we only need to change the sender for the list action form
     if sender is not None:
         m.sender = sender
-    mailer.send(m)
-    if current_app.debug:
-        current_app.logger.info("sent email to %s: %s / %s", to, subj, msg)
+    try:
+        mailer.send(m)
+        if current_app.debug:
+            current_app.logger.info("sent email to %s: %s / %s", to, subj, msg)
+        return True
+    except SMTPException as e:
+        current_app.logger.warn(e)
+        return False
