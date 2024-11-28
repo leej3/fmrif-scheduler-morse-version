9581dbed49b9bdc7bd935d397a0f6413fd693409
V. R. <for_usenet@yahoo.com>
2024-11-27 21:19:16 -0500

    Adding alternate way to run scheduler app via flask directly, from a Python virtual environment.

--- scheduler/dev-server.sh
+++ scheduler/dev-server.sh
@@ -1,2 +1,9 @@
+
 #!/bin/bash
-pipenv run flask run
+
+# Running from pipenv
+# pipenv run flask run # -h 0.0.0.0 # to run on all interfaces
+
+# Running from virtual environment
+flask run              # -h 0.0.0.0 # again, to run on all interfaces
+
