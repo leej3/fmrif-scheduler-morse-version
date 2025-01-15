fdcd7c10a1569cd0e01d8a282ea45fd2ee1a2ceb
leej3 <johnleenimh@gmail.com>
2025-01-15 14:01:32 +0000

    run alembic from migrations directory

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
 
-# Run database migrations
+# Run database migrations from the migrations directory
+cd migrations
 alembic upgrade head
+cd ..
 
 # Start the application
 flask run --host 0.0.0.0 --port 5051
